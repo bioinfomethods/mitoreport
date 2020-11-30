18e7f462163e96f2d85ed2d4f83edaca1898a8c6
Tommy Li <tommy.li@firefire.co>
2020-11-30 23:29:56 +1100

    fix: Fixed test to ensure variant detail to show does exist after new filters

--- ui/tests/unit/App.spec.js
+++ ui/tests/unit/App.spec.js
@@ -46,7 +46,7 @@ describe('App.vue', () => {
   it('renders VariantDetails when navigate to a variant row', async () => {
     router.push({
       name: 'variantDetails',
-      params: { variantId: 'chrM-152-T-C' },
+      params: { variantId: 'chrM-310-T-TC' },
     })
     await underTest.vm.$nextTick()
 
