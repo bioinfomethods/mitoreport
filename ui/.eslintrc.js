c709f155b73d40ad199a732082cbc0ee53904cd0
tommy.li <tommy.li@firefire.co>
2021-04-26 17:34:18 +1000

    style: linting

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     ],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-unused-vars": "warn",
+    'no-unused-vars': 'warn',
   },
   overrides: [
     {
