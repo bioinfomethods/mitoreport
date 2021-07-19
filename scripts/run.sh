a5deb56b3707aed9d77fb3cd951ebb2f02409fe9
David Ma <david.ma@mcri.edu.au>
2021-07-19 12:46:25 +1000

    Update run.sh with APP_ARCHIVE_VERSION and MitoMap Download

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
 PROJECT_DIR=$(pwd)
+TODAY=$(date +"%Y%m%d")
+APP_ARCHIVE_VERSION=$(git describe --abbrev=0)
 
 echo "Building mitoreport"
 echo "Project directory: $PROJECT_DIR"
 echo "Java home: $JAVA_HOME"
 
-./gradlew
+java -version
+
+# ./gradlew
 # ./gradlew compileJava jar 
 
 # ./gradlew -x check -x copyUiBuildToMitoOut -x buildGngs -x clean -x buildHaplogrepCmd -x test
@@ -15,14 +19,20 @@ echo "Java home: $JAVA_HOME"
 
 # ./gradlew -x test
 
-java -jar $PROJECT_DIR/build/libs/mitoreport-0.1.0-all.jar mito-report \
+
+# Download MITOMAP Annotations 
+# java -jar "${PROJECT_DIR}/build/libs/mitoreport-${APP_ARCHIVE_VERSION}-all.jar" mito-map-download \
+#   --output "${PROJECT_DIR}/test_fixtures/mito_map_annotations_${TODAY}.json"
+
+
+
+java -jar ${PROJECT_DIR}/build/libs/mitoreport-${APP_ARCHIVE_VERSION}-all.jar mito-report \
   -d -sample "15G002035-GM12878K_20pc_10kb_200" \
-  -vcf $PROJECT_DIR/test_fixtures/variants/15G002035.unshifted.contamination.filtering.intermediatefilter.norm.dedup.mito_vep.vcf.gz \
-  --annotations $PROJECT_DIR/test_fixtures/mtDNAanalysis_annotations_20170501.csv \
-  --mito-annotations $PROJECT_DIR/test_fixtures/mito_map_annotations_20201207.json \
-  --gnomad-vcf $PROJECT_DIR/test_fixtures/gnomad.genomes.v3.1.sites.chrM.vcf.bgz \
-  "$PROJECT_DIR/test_fixtures/align/15G002035-GM12878K_20pc_10kb_200.unshifted.bam" $PROJECT_DIR/test_fixtures/controls/*.bam
+  -vcf ${PROJECT_DIR}/test_fixtures/variants/15G002035.unshifted.contamination.filtering.intermediatefilter.norm.dedup.mito_vep.vcf.gz \
+  --annotations ${PROJECT_DIR}/test_fixtures/mtDNAanalysis_annotations_20170501.csv \
+  --mito-annotations ${PROJECT_DIR}/test_fixtures/mito_map_annotations_20201207.json \
+  --gnomad-vcf ${PROJECT_DIR}/test_fixtures/gnomad.genomes.v3.1.sites.chrM.vcf.bgz \
+  "${PROJECT_DIR}/test_fixtures/align/15G002035-GM12878K_20pc_10kb_200.unshifted.bam" ${PROJECT_DIR}/test_fixtures/controls/*.bam
 
-
-#groovy scripts/FileMeta.groovy $PROJECT_DIR/test_fixtures/gnomad.genomes.v3.1.sites.chrM.vcf.bgz
+#groovy scripts/FileMeta.groovy ${PROJECT_DIR}/test_fixtures/gnomad.genomes.v3.1.sites.chrM.vcf.bgz
 
