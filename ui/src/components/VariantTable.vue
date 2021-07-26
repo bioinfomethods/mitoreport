56f0d70d08346cc48ccb839febd6223422962087
David Ma <david.ma@mcri.edu.au>
2021-07-26 15:21:59 +1000

    Feature(mitoreport#17) Add Frequency Alert to MitoTIP column

--- ui/src/components/VariantTable.vue
+++ ui/src/components/VariantTable.vue
@@ -474,6 +474,11 @@
                     style="color: #fa0001;"
                     >mdi-arrow-up-thick</v-icon
                   >
+                  <v-icon
+                    v-if="freqAlert(item)"
+                    style="color: #008001; margin-left: -8px;"
+                    >mdi-asterisk</v-icon
+                  >
                 </span>
               </template>
               <ul>
@@ -508,6 +513,10 @@
                   >
                   <v-icon style="color: #008001;">mdi-arrow-down-thick</v-icon>
                   Likely benign
+                </li>
+                <li>
+                  <v-icon style="color: #008001;">mdi-asterisk</v-icon>Frequency
+                  alert
                 </li>
               </ul>
             </v-tooltip>
@@ -1340,6 +1349,25 @@ export default {
 
     toggleHaplodata() {
       document.getElementById('app').classList.toggle('showHaplodata')
+    },
+
+    // A "Frequency Alert" asterisk is added to the MitoTIP ranking if a variant is found (1) at >1% in at least one of the macro-lineages L, M, or N, or (2) at >10% in at least one of the major (single letter) haplogroups, with minimum # sequences = 10. Note that the frequency alert is for information purposes only. It does not confirm a benign status, especially if a variant is found outside of the high-frequency haplogroup.
+    // https://mitomap.org/foswiki/bin/view/MITOMAP/MitoTipInfo#A_42New_Additional_Features_42
+    freqAlert(item) {
+      if (this.mitoTipCount && this.mitoTipCount >= 10) {
+        if (
+          _.includes('LMN', this.getFirstHaplogroup) &&
+          item.mitoTipFreq >= 1
+        ) {
+          return true
+        } else if (
+          this.getFirstHaplogroup.length === 1 &&
+          item.mitoTipFreq >= 10
+        ) {
+          return true
+        }
+      }
+      return false
     },
 
     currentItems(items) {
