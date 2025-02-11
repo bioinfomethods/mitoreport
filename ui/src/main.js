a1f7faaa45f6f3f17575785d2ec4c4521c2a30bf
Simon Sadedin <ssadedin@gmail.com>
2025-02-11 18:22:35 +1100

    rename router js to router.js for clarity

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,11 +1,11 @@
 import vuetify from '@/plugins/vuetify'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router.js'
 import store from './store'
 
 new Vue({
-  store,
+  store: store.store,
   router,
   vuetify,
   render: h => h(App),
