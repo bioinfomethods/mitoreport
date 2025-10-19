742e454f7cb4b50f9f86fdc82a69886636bddae9
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-10-19 23:19:34 +1100

    declare loadScripts as global so it is not reported as error

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   env: {
     node: true,
   },
+  globals: {
+    loadScripts: 'readonly',
+  },
   extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
     parser: 'babel-eslint',
